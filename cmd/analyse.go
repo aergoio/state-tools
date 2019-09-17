be1902d9a0327ba45edc9b144ae3dabfedfe3896
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2019-09-17 15:38:55 +0900

    [cli] analyse state with block height parameter

--- cmd/analyse.go
+++ cmd/analyse.go
@@ -6,6 +6,7 @@ import (
 	"path"
 
 	"github.com/aergoio/aergo-lib/db"
+	"github.com/aergoio/aergo/types"
 	"github.com/aergoio/state-tools/stool"
 	"github.com/mr-tron/base58/base58"
 	"github.com/spf13/cobra"
@@ -14,11 +15,13 @@ import (
 var (
 	contractTrie bool
 	root         string
+	blockHeight  uint64
 )
 
 func init() {
 	analyseCmd.Flags().BoolVar(&contractTrie, "contractTrie", false, "The trie being queried is a contract trie")
-	analyseCmd.Flags().StringVarP(&root, "root", "r", "", "Root of the Aergo trie to analyse/snapshot")
+	analyseCmd.Flags().StringVarP(&root, "root", "r", "", "Root of the Aergo trie to analyse")
+	analyseCmd.Flags().Uint64VarP(&blockHeight, "blockHeight", "b", 0, "Block height to analyse")
 	rootCmd.AddCommand(analyseCmd)
 }
 
@@ -37,6 +40,18 @@ func execAnalyse(cmd *cobra.Command, args []string) {
 	}
 	store := db.NewDB(db.BadgerImpl, statePath)
 
+	if len(root) != 0 && blockHeight != 0 {
+		fmt.Println("choose between root and blockHeight flags")
+		return
+	}
+	if contractTrie && len(root) == 0 {
+		fmt.Println("must provide storage root for analysing contract trie")
+		return
+	}
+
+	chainPath := path.Join(dbPath, "chain")
+	chainStore := db.NewDB(db.BadgerImpl, chainPath)
+
 	// Get state root
 	var rootBytes []byte
 	var err error
@@ -46,18 +61,24 @@ func execAnalyse(cmd *cobra.Command, args []string) {
 			fmt.Println(err)
 			return
 		}
+	} else if blockHeight != 0 {
+		heightBytes := types.BlockNoToBytes(blockHeight)
+		rootBytes, err = getTrieRoot(chainStore, heightBytes)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	} else {
 		// query latest state root in chain db
-		chainPath := path.Join(dbPath, "chain")
-		chainStore := db.NewDB(db.BadgerImpl, chainPath)
 		rootBytes, err = getLatestTrieRoot(chainStore)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		chainStore.Close()
 	}
+	chainStore.Close()
 
+	fmt.Println("\nAnalysing state with root: ", base58.Encode(rootBytes))
 	sa := stool.NewStateAnalysis(store, counterOn, !contractTrie, 10000)
 	err = sa.Analyse(rootBytes)
 	if err != nil {
@@ -67,5 +88,5 @@ func execAnalyse(cmd *cobra.Command, args []string) {
 	store.Close()
 
 	displayResults(sa, contractTrie)
-	displayFolderSizes(dbPath, "Size information:")
+	displayFolderSizes(dbPath, "Current latest state size information:")
 }
